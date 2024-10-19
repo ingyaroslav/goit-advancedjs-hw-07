618724ba339c14fddedaca763ff315890efd5316
ingyaroslav <ingyaroslav@gmail.com>
2024-10-19 18:01:14 +0300

    +

--- src/3.ts
+++ src/3.ts
@@ -7,6 +7,16 @@
   Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
 
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
+interface ISpellCaster {
+  castSpell(): void;
+}
+
 // реалізація класу Wizard
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
@@ -14,15 +24,12 @@ class Wizard implements ICharacter, ISpellCaster {
       throw new Error('Level too low');
     }
   }
-
   introduce(phrase: string): void {
     console.log(phrase + ', ' + this.name);
   }
-
   castSpell(): void {
     console.log('Casting a spell, behold my power!');
   }
-
   levelUp(): void {
     this.level++;
     console.log(`Level up! New level is ${this.level}`);
@@ -31,7 +38,6 @@ class Wizard implements ICharacter, ISpellCaster {
 
 // тестування класу
 const wizard = new Wizard('Merlin', 15);
-
 wizard.introduce('I am the mighty wizard');
 wizard.castSpell();
 wizard.levelUp();  // Level up! New level is 16
